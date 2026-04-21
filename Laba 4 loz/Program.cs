de6a1bf95d180c93a27341dacda27ead6e18caf1
Clarence Buttowski <zubenkovavan@gmail.com>
2026-04-21 16:46:00 +0300

    Remove unnecessary blank line in Program.cs

--- Laba 4 loz/Program.cs
+++ Laba 4 loz/Program.cs
@@ -14,7 +14,7 @@ namespace Laba4Loz
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
